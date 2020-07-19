b4af703c72f2ccc58cb1fff177519401272bd3b8
paxmeny <36705016+paxmeny@users.noreply.github.com>
2020-07-19 13:29:04 -0400

    Add files via upload

---
+++ hw2.sprint1.py
@@ -0,0 +1,13 @@
+import array as arr
+x=arr.array([7,6,5,4])
+y=arr.array([4,3,2,1])
+int total=0
+int count=array.len(x)
+for array.len(x)
+    total+=x(count-1)
+    total+=y(count-1)
+    count--
+    if count==0
+        close()
+print(total)
+return
